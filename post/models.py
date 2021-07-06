d220db7ce2a1635ad543fe9797159a1a4c649b59
Aelita <aelitamirlanova72@gmail.com>
2021-07-06 20:54:18 +0600

    change settings, create post and create models

---
+++ post/models.py
@@ -0,0 +1,48 @@
+from django.db import models
+from django.conf import settings
+
+
+class Tag(models.Model):
+
+    title = models.CharField(max_length=100, verbose_name='Название')
+    slug = models.SlugField(verbose_name='слаг')
+
+    def __str__(self):
+        return self.title
+
+    class Meta:
+        verbose_name = 'Тэг'
+        verbose_name_plural = 'Тэги'
+
+
+class Category(models.Model):
+
+    title = models.CharField(max_length=150, verbose_name='Название')
+    slug = models.SlugField(verbose_name='слаг')
+
+    def __str__(self):
+        return self.title
+
+    class Meta:
+        verbose_name = 'Категория'
+        verbose_name_plural = 'Категории'
+
+
+class Post(models.Model):
+
+    title = models.CharField(max_length=250, verbose_name='Заголовок')
+    content = models.TextField(verbose_name='Контент')
+    created_at = models.DateTimeField(auto_now_add=True, verbose_name='Дата создания')
+    author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.PROTECT, verbose_name='Автор')
+    category = models.ForeignKey(Category, on_delete=models.CASCADE, verbose_name='Категория')
+    tags = models.ManyToManyField(Tag, verbose_name='Тэги')
+    slug = models.SlugField(verbose_name='слаг')
+    views = models.PositiveIntegerField(default=0)
+
+    def __str__(self):
+        return self.title
+
+    class Meta:
+        verbose_name = 'Пост'
+        verbose_name_plural = 'Посты'
+
