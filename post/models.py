eb7f3b51a008911b9917445f18ea125201081a83
Aelita <aelitamirlanova72@gmail.com>
2021-07-06 22:05:15 +0600

    add admin, change post fields, settings, add get_photo, set MEDIA_ROOT

--- post/models.py
+++ post/models.py
@@ -32,12 +32,15 @@ class Post(models.Model):
 
     title = models.CharField(max_length=250, verbose_name='Заголовок')
     content = models.TextField(verbose_name='Контент')
-    created_at = models.DateTimeField(auto_now_add=True, verbose_name='Дата создания')
-    author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.PROTECT, verbose_name='Автор')
-    category = models.ForeignKey(Category, on_delete=models.CASCADE, verbose_name='Категория')
+    created_at = models.DateTimeField(auto_now_add=True, verbose_name='Опубликовано')
+    author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.PROTECT, verbose_name='Автор', 
+                               related_name='posts')
+    category = models.ForeignKey(Category, on_delete=models.CASCADE, verbose_name='Категория', 
+                               related_name='posts')
     tags = models.ManyToManyField(Tag, verbose_name='Тэги')
     slug = models.SlugField(verbose_name='слаг')
-    views = models.PositiveIntegerField(default=0)
+    views = models.PositiveIntegerField(default=0, verbose_name='Количество просмотров')
+    photo = models.ImageField(upload_to='post/%Y/%m/%d/', verbose_name='Фото', null=True, blank=True)
 
     def __str__(self):
         return self.title
